25f8ea936a349c687b871f9852cd4eee6df6c651
Tim Fuehner <tim.fuehner@iisb.fraunhofer.de>
2016-04-20 22:51:55 +0200

    - added menu and shortcut creation

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -5,7 +5,16 @@
 # author:	Tim Fuehner <tim.fuehner@iisb.fraunhofer.de>
 # $Id$
 #
-
+#!/bin/bash
+mkdir -p "$PREFIX/Menu"
+if [ $OSX_ARCH ]
+then
+	cp "$RECIPE_DIR/menu-osx.json" "$PREFIX/Menu"
+	cp "$RECIPE_DIR/app.icns" "$PREFIX/Menu"
+else
+	cp "$RECIPE_DIR/menu-linux.json" "$PREFIX/Menu"
+	#cp "$RECIPE_DIR/app.svg" "$PREFIX/Menu"
+fi
 $PYTHON setup.py install
 
 
