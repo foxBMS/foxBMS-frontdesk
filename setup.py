5e64fb75e2721cd7f0873f08f14fab8df4ca45d2
Fuehner Tim <fuehner@iisb.local>
2016-05-04 11:50:09 +0200

    - version bump 0.5 - added gui script generation

--- setup.py
+++ setup.py
@@ -6,15 +6,24 @@ $Id$
 
 import setuptools
 from setuptools import setup, find_packages
+import sys
+
+if sys.platform.startswith('win'):
+    _entry_points = {
+            'gui_scripts': ['foxbms = foxbms.foxfdd:main']
+            }
+else:
+    _entry_points = {}
 
 setup(
         name = "foxbms",
-        version = "0.1",
+        version = "0.5",
         packages = find_packages(),
-        author = "Tim Fuehner",
-        author_email = "tim.fuehner@iisb.fraunhofer.de",
+        author = "foxBMS",
+        author_email = "info@foxbms.org",
         package_data={'foxbms': ['xrc/*',]},
-        zip_safe = False
+        zip_safe = False,
+        entry_points=_entry_points
         )
 
 
